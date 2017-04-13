ff039d75fd8ededdc04cd3cd7cf8ab5ed0ba56c0
Sagar Trivedi <sagar.trivedi@gmail.com>
2017-04-13 12:37:44 +0530

    adding constant in SocialSharingWebKeys and Language

---
+++ add-sharing-category-layout-portlet/src/main/java/ka/sharing/SocialSharingWebKeys.java
@@ -0,0 +1,8 @@
+package ka.sharing;
+
+public class SocialSharingWebKeys {
+	public final static String SHARE_TAB="share";
+	public final static String SHARE_JSP_PATH="/portal_settings/social_sharing.jsp";
+	public final static String SHARE_JSP="social_sharing";
+	public final static String DEFAULT_POSITION_LEFT = "left";
+}
